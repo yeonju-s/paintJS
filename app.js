67e99f4e16e6ee7be5796d60d16c797dd690ed02
yeonju-s <nineelephant@naver.com>
2022-04-07 23:18:58 +0900

    add 'FILL canvas' mode

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
-ctx.strokeStyle = '#2c2c2c';
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.heigth = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -24,18 +35,50 @@ function onMouseMove(event){
     } else {
         ctx.lineTo(x, y)
         ctx.stroke();
-        ctx.closePath();
     }
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
+function handleRangeChane(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill"; 
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick() {
+    if(filling){
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    };
+}
 
 if (canvas){
     canvas.addEventListener('mousemove', onMouseMove)
     canvas.addEventListener('mousedown', startPainting)
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
+}
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+
+if(range) {
+    range.addEventListener('input', handleRangeChane)
+}
+
+if(mode){
+    mode.addEventListener('click', handleModeClick);
 }
